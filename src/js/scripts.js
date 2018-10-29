d25c6d47a6bdd344ee39437fc8a24a98a4e33a47
neiljoshua <neiljoshua@me.com>
2018-10-29 07:44:41 -0400

    Adding editorcobfig file  and editing js file

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,7 +4,7 @@ var list = [];
 var pageList = [];
 var currentPage = 1;
 var numberPerPage = 10;
-var	numberOfPages = 1;
+var numberOfPages = 1;
 var fragmentGallery = document.createDocumentFragment();
 
 var request = new XMLHttpRequest();
@@ -12,160 +12,160 @@ var request = new XMLHttpRequest();
 request.open('GET', flickrApi, true);
 request.onload = function () {
 
-	var data = JSON.parse(this.response);
+  var data = JSON.parse(this.response);
 
-	if (request.status >= 200 && request.status < 400) {
+  if (request.status >= 200 && request.status < 400) {
 
-		var photos = data.photos.photo;
+    var photos = data.photos.photo;
 
-		photos.forEach( function(photo) {
-			var imageContainer = document.createElement('div'),
-					image = document.createElement('img');
+    photos.forEach( function(photo) {
+      var imageContainer = document.createElement('div'),
+          image = document.createElement('img');
 
-			imageContainer.setAttribute('class','thumbnail');
-			image.setAttribute('data-image', photo.url_m);
-			image.setAttribute('class','thumbnail__image');
-			imageContainer.appendChild(image);
-			list.push(imageContainer);
-			fragmentGallery.appendChild(imageContainer);
+      imageContainer.setAttribute('class','thumbnail');
+      image.setAttribute('data-image', photo.url_m);
+      image.setAttribute('class','thumbnail__image');
+      image.addEventListener('click',toggleModal);
+      imageContainer.appendChild(image);
+      list.push(imageContainer);
+      fragmentGallery.appendChild(imageContainer);
 
-		});
+    });
 
-	} else {
+  } else {
 
-		var errorMessage = document.createElement('marquee');
+    var errorMessage = document.createElement('marquee');
 
-		errorMessage.textContent = 'Api is not working';
-		galleryPhoto.appendChild(errorMessage);
+    errorMessage.textContent = 'Api is not working';
+    galleryPhoto.appendChild(errorMessage);
 
-	}
+  }
 
-	if(request.readyState) {
-		loadImages();
-	}
+  if(document.readyState) {
+    loadImages();
+  }
 
-	loadPages();
+  loadPages();
 
 };
 
 request.send();
 
-document.addEventListener('click', function(event) {
+window.onload = function() {
+  var buttons = document.querySelector('.button'),
+      modal = document.querySelector('.modal');
 
-	var clickElem = event.target.classList.value;
+  buttons.addEventListener('click', toggleButtons);
+  modal.addEventListener('click', toggleModal);
+}
 
-	switch (clickElem){
-		case 'button__prev':
-			previousPage();
-			break;
-		case 'button__next':
-			nextPage();
-			break;
-		case 'button__first':
-			firstPage();
-			break;
-		case 'button__last':
-			lastPage();
-			break;
-		case 'thumbnail__image':
-			openModal(event);
-			break;
-		case 'close__button':
-			closeModal(event);
-	}
+function toggleButtons(event) {
 
-});
+  var className = event.target.classList.value;
+
+  switch (className){
+    case 'button__prev':
+      previousPage();
+      break;
+    case 'button__next':
+      nextPage();
+      break;
+    case 'button__first':
+      firstPage();
+      break;
+    case 'button__last':
+      lastPage();
+      break;
+  }
+
+}
+
+function toggleModal(event) {
+  var modal = document.querySelector('.modal'),
+      modalImage = document.querySelector('.modal__image'),
+      imagesrc = event.target.dataset.image,
+      className = event.target.className;
+
+  modalImage.setAttribute('src', '');
+  if( className == 'thumbnail__image') {
+    modalImage.setAttribute('src', imagesrc);
+  }
+  modal.classList.toggle('show__modal');
+
+}
 
 function loadImages() {
-	var photosReady = fragmentGallery.querySelectorAll('.thumbnail__image');
+  var photosReady = fragmentGallery.querySelectorAll('.thumbnail__image');
 
-	photosReady.forEach( function(photo) {
-		setTimeout(function(){
-			var source = photo.dataset.image;
+  photosReady.forEach( function(photo) {
+    setTimeout(function(){
+      var source = photo.dataset.image;
 
-			photo.setAttribute('src', source);
+      photo.setAttribute('src',source);
+    }, 7 * photosReady.length );
 
-		}, 7 * photosReady.length );
-
-	});
+  });
 }
 
-function openModal(event) {
-	var modal = document.querySelector('.modal'),
-			modalImage = document.querySelector('.modal__image'),
-			imagesrc = event.target.dataset.image;
 
-	modalImage.setAttribute('src', imagesrc);
-	modal.classList.add('show__modal');
-
-}
-
-function closeModal(event) {
-	var modal = document.querySelector('.modal'),
-			modalImage = document.querySelector('.modal__image');
-
-	modal.classList.remove('show__modal');
-	modalImage.setAttribute('src', '');
-}
-
-function makePage() {
-	numberOfPages = getNumberOfPages();
+function createPage() {
+  numberOfPages = getNumberOfPages();
 }
 
 function getNumberOfPages() {
-	return Math.ceil(list.length / numberPerPage);
+  return Math.ceil(list.length / numberPerPage);
 }
 
 function nextPage() {
-	currentPage += 1;
-	loadPage();
+  currentPage += 1;
+  loadPage();
 }
 
 function previousPage() {
-	currentPage -= 1;
-	loadPage();
+  currentPage -= 1;
+  loadPage();
 }
 
 function firstPage() {
-		currentPage = 1;
-		loadPage();
+    currentPage = 1;
+    loadPage();
 }
 
 function lastPage() {
-		currentPage = numberOfPages;
-		loadPage();
+    currentPage = numberOfPages;
+    loadPage();
 }
 
 function loadPage() {
-	var begin = ((currentPage - 1) * numberPerPage ),
-			images = document.getElementsByClassName('thumbnail__image'),
-			end = begin + numberPerPage;
+  var firstpage = ((currentPage - 1) * numberPerPage ),
+      images = document.getElementsByClassName('thumbnail__image'),
+      lastPage = firstpage + numberPerPage;
 
-	pageList = list.slice(begin, end);
+  pageList = list.slice(firstpage, lastPage);
 
-	drawPage();
-	checkButtons();
+  drawPage();
+  checkButtons();
 }
 
 function drawPage() {
-	var gallery = document.getElementById('gallery');
-	while (gallery.firstChild) {
-		gallery.removeChild(gallery.firstChild);
-	}
-	for (i = 0; i < pageList.length; i++) {
-		document.getElementById('gallery').appendChild(pageList[i]);
-	}
+  var gallery = document.getElementById('gallery');
+  while (gallery.firstChild) {
+    gallery.removeChild(gallery.firstChild);
+  }
+  for (i = 0; i < pageList.length; i++) {
+    document.getElementById('gallery').appendChild(pageList[i]);
+  }
 }
 
 function checkButtons() {
 
-	document.querySelector('.button__next').disabled = currentPage == numberOfPages ? true : false;
-	document.querySelector('.button__prev').disabled = currentPage == 1 ? true : false;
-	document.querySelector('.button__first').disabled = currentPage == 1 ? true : false;
-	document.querySelector('.button__last').disabled = currentPage == numberOfPages ? true : false;
+  document.querySelector('.button__next').disabled = currentPage == numberOfPages ? true : false;
+  document.querySelector('.button__prev').disabled = currentPage == 1 ? true : false;
+  document.querySelector('.button__first').disabled = currentPage == 1 ? true : false;
+  document.querySelector('.button__last').disabled = currentPage == numberOfPages ? true : false;
 }
 
 function loadPages() {
-	makePage();
-	loadPage();
+  createPage();
+  loadPage();
 }
